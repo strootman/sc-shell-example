c962b586c2d16d4889c228c67d1cdc3dbcb5088e
Jonathan Strootman <jstroot@iplantcollaborative.org>
2016-03-29 11:18:20 -0700

    FIXED and ran setupProject.sh

--- setupProject.sh
+++ setupProject.sh
@@ -13,7 +13,7 @@ mkdir -v data code output data/archive
 cd data
 
 # Fetch the zip file
-ZIP_FILE_URL="https://github.com/strootman/sc-shell-example/archive/rawSplitCountries.zip"
+ZIP_FILE_URL="http://github.com/strootman/sc-shell-example/archive/rawSplitCountries.zip"
 ZIP_FILE_NAME="rawSplitCountries.zip"
 
 echo Downloading zipped data file from $ZIP_FILE_URL
